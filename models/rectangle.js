ff2f1d7174bf7a97adb839f25120f8867623aba7
Tuyenrickchoi <gaxuongban@gmail.com>
2024-11-07 23:46:47 +0700

    first commit

---
+++ models/rectangle.js
@@ -0,0 +1,14 @@
+// model để tính chu vi hình chữ nhật
+class Rectangle {                        
+    constructor(width, height) {                        
+        this.width = width;                        
+        this.height = height;                        
+    }                        
+                            
+    getPerimeter() {                        
+        return 2 * (this.width + this.height);                        
+    }       
+                
+}                        
+                            
+module.exports = Rectangle;
